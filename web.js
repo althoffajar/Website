73e4d0408564e167da62d626bbbb21654249b39c
althoffajar <althoffajar3@gmail.com>
2023-06-27 18:32:36 +0100

    Daily Commits

--- web.js
+++ web.js
@@ -1,4 +1,4 @@
-// Main Page
+// ------------------------------------------***Main Page**------------------------------------------------
 //const selection = document.querySelector('selection')
 //const selectionButton = document.querySelector('.navigation_selection')
 
@@ -7,26 +7,45 @@
 //    selectionButton.classList.toggle('active');
 //})
 
-// Login Page
-let CreateButton = document.getElementById("CreateButton");
-let SignInButton = document.getElementById("SignInButton");
-let UserInput = document.getElementById("UserInput");
-let FormHeader = document.getElementById("FormHeader");
-let LoginOrCreateLink = document.getElementById("LoginOrCreateLink");
 
-LoginOrCreateLink.onclick = function(){ 
-    if(UserInput.style.maxHeight == "60px"){
-        UserInput.style.maxHeight = "0";
-        CreateButton.classList.add("disable");
-        SignInButton.classList.remove("disable");
-        FormHeader.innerHTML = "Log In";
-        LoginOrCreateLink.innerHTML = "Create an account here";
-    }
-    else{
-        UserInput.style.maxHeight = "60px";
-        CreateButton.classList.remove("disable");
-        SignInButton.classList.add("disable");
-        FormHeader.innerHTML = "Create an Account";
-        LoginOrCreateLink.innerHTML = "Already Have An Account? Go Login!";
-    }
-}
+
+
+// ------------------------------------------***Login Page**------------------------------------------------
+
+
+
+function messageSetting(Element, type, output) {
+    const messageElement = Element.querySelector(".Message_Failed");
+
+    messageElement.textContent = message.textContent = output;
+    output.classList.remove("Message_Success", "Message_Failed");
+    output.classList.add(`Message_${type}`)
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    const SignIn = document.querySelector("#SignInPage");
+    const CreateAccount = document.querySelector("#CreateAccountPage");
+
+    //document.querySelector("#ButtonSignInPage") document.querySelector("#LinkSignIn")
+    document.querySelector("#ButtonSignInPage").addEventListener("click", a => {
+        a.preventDefault();
+        CreateAccount.classList.add("Inputs_DisplayNone");
+        SignIn.classList.remove("Inputs_DisplayNone");
+    });
+
+    //document.querySelector("#ButtonToCreateAccount") document.querySelector("#LinkCreateAccount")
+    document.querySelector("#ButtonToCreateAccount").addEventListener("click", a =>{
+        a.preventDefault();
+        SignIn.classList.add("Inputs_DisplayNone");
+        CreateAccount.classList.remove("Inputs_DisplayNone");
+    });
+
+    SignIn.addEventListener("submit", a =>{
+        a.preventDefault();
+
+        //Perform Login
+
+        messageSetting(SignIn, "error", "invalid username/password combination");
+    })
+
+});
